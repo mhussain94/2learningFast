faa8115eddcf1121a6649a44bdcdd38da97266d6
Argedor <Argedor@Argedors-MacBook-Pro.local>
2021-03-26 16:46:44 +0300

    more coding

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response
 import uvicorn
 from pydantic import BaseModel
 import schemas, model #Importing from the same directory
 from database import SessionLocal, engine
 from sqlalchemy.orm import Session
-
-
 
 
 app = FastAPI()
@@ -20,7 +18,7 @@ def get_db():
 
 
 #Depends to create connection to db by creating a get_db function
-@app.post('/blog')
+@app.post('/blog', status_code=201) #status code for something created
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = model.Blog(title = request.title, body = request.body)
     db.add(new_blog) #add the blog
@@ -33,12 +31,13 @@ def get_blogs(db: Session = Depends(get_db)):
     blogs = db.query(model.Blog).all() #model.Blog is the tablename!
     return blogs
 
-@app.get('/blog/{id}')
-def get_single_blog(id, db: Session = Depends(get_db)):
+@app.get('/blog/{id}', status_code=200)
+def get_single_blog(id,response: Response, db: Session = Depends(get_db)):
     blog = db.query(model.Blog).filter(model.Blog.id == id).first()
     if blog != None:
         return blog
     elif blog == None:
+        response.status_code =400 #To generate custom responses!!, need to provide response:Response in params
         return 'no blog found'
 
 if __name__ == '__main__': 
