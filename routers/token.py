599e55d6c55ca1037a7be79c466be01aeec6ad8b
Argedor <Argedor@Argedors-MacBook-Pro.local>
2021-03-29 17:28:08 +0300

    .

--- routers/token.py
+++ routers/token.py
@@ -1,8 +1,8 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 
-
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+#secret key created with "openssl rand -hex 32"
+SECRET_KEY = "b116705ddd2dbd2f7461a6fba503dab791902bf50e3b14046c49768005cc230a"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
